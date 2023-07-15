7e062e62ca7f72d6acbe29e5e9c9fe20d0280d64
Pranath04 <medanarayanapranath@gmail.com>
2023-07-15 23:55:51 +0530

    first commit

--- Reversearray.java
+++ Reversearray.java
@@ -5,35 +5,28 @@ public class Reversearray{
 	public static void main(String []args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter array size");
-        int n= scanner.nextInt();
-        System.out.println("Enter array Elements");
-        int array[]=new int[n];
-        for(int i=0;i<n;i++){
-            array[i]=scanner.nextInt();
-        }
-        System.out.println("Enter how many times array need to rotate");
-        int b= scanner.nextInt();
-        scanner.close();
-        if (b>n){
-            b%=n;
-        }
-        rotateArray(array,0,n-1);
-        rotateArray(array,0,b-1);
-        rotateArray(array,b,n-1);
-        for(int i=0;i<n;i++){
-            System.out.println(array[i]);
-        }
-    }
-
-    static void rotateArray(int a[],int start,int end){
-        int i=start,j=end,temp;
-        while(i<j){
-            temp=a[i];
-            a[i]=a[j];
-            a[j]=temp;
-            i++;j--;
-        }
-    }
-		
-	
+		int arrsize = scanner.nextInt();
+		System.out.println("Enter array elements");
+		int arr[]=new int[arrsize];
+		for(int i=0;i<arrsize;i++) {
+			arr[i]=scanner.nextInt();
+		}
+		int r=0;
+		int b = scanner.nextInt();
+		scanner.close();
+		int ans[] = reverseing(r,arrsize,arr);
+		for(int i=0;i<ans.length;i++){
+	        System.out.println(ans[i]);
+	    }
+	}
+	static int[] reverseing(int r,int arrsize,int arr[]) {
+		while(arrsize-1>r) {
+			int temp=arr[arrsize-1];
+            arr[arrsize-1]=arr[r];
+            arr[r]=temp;
+			r++;
+			arrsize--;
+ 		}
+		return arr;
+	}
 }
